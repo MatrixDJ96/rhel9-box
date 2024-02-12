51e5e67e5ea6ca737705f39f54cc4c69f9444617
Mattia Rombi <mattia.rombi@siatass.com>
2024-02-12 12:00:00 +0100

    Add Apache and MySQL provisioning

--- config/provision/composer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://getcomposer.org/download/latest-stable/composer.phar
-mv -f composer.phar /bin/composer
-chmod +x /bin/composer
--- config/provision/java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum -y install \
-  java-11-openjdk \
-  java-21-openjdk \
-  java-11-openjdk-devel \
-  java-21-openjdk-devel
