e46709f1bd22431bbe319e95c1bbd4cbc06344dc
Mattia Rombi <mattyro96@gmail.com>
2026-07-31 14:59:06 +0200

    Pin the MySQL install to the requested series

--- config/provision/mysql.sh
+++ config/provision/mysql.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-yum -y install https://dev.mysql.com/get/mysql84-community-release-el9-3.noarch.rpm
+MYSQL_VERSION="${MYSQL_VERSION:-8.4}"
 
-yum config-manager --disable mysql80-community
-yum config-manager --enable mysql-8.4-lts-community
+yum -y install "https://dev.mysql.com/get/mysql${MYSQL_VERSION//./}-community-release-el9.rpm"
+
+# the release package enables its own LTS series: keep only the wanted one
+yum config-manager --disable 'mysql*'
+yum config-manager --enable \
+  "mysql-${MYSQL_VERSION}-lts-community" \
+  "mysql-tools-${MYSQL_VERSION}-lts-community" \
+  mysql-connectors-community
 
 yum -y install mysql-community-server
+
+# fail on any series but the wanted one
+INSTALLED_VERSION="$(rpm -q --queryformat '%{VERSION}' mysql-community-server)"
+if [[ "${INSTALLED_VERSION}" != "${MYSQL_VERSION}."* ]]; then
+  echo "MySQL ${MYSQL_VERSION} expected, ${INSTALLED_VERSION} installed" >&2
+  exit 1
+fi
 
 install -m 0755 -o root -g root \
   /vagrant/config/mysql/entrypoint.sh \
@@ -25,14 +38,16 @@ install -D -m 0644 -o root -g root \
 
 # the "socket" value in the Oracle packages is set to "/var/lib/mysql" which isn't a great place for the socket (we want it in "/var/run/mysqld" instead)
 # https://github.com/docker-library/mysql/pull/680#issuecomment-636121520
-grep -F 'socket=/var/lib/mysql/mysql.sock' /etc/my.cnf
 sed -i 's!^socket=.*!socket=/var/run/mysqld/mysqld.sock!' /etc/my.cnf
 grep -F 'socket=/var/run/mysqld/mysqld.sock' /etc/my.cnf
-{ echo '[client]'; echo 'socket=/var/run/mysqld/mysqld.sock'; } >> /etc/my.cnf
+if ! grep -qF '[client]' /etc/my.cnf; then
+  { echo '[client]'; echo 'socket=/var/run/mysqld/mysqld.sock'; } >> /etc/my.cnf
+fi
 
 # make sure users dumping files in "/etc/mysql/conf.d" still works
-! grep -F '!includedir' /etc/my.cnf
-{ echo; echo '!includedir /etc/mysql/conf.d/'; } >> /etc/my.cnf
+if ! grep -qF '!includedir' /etc/my.cnf; then
+  { echo; echo '!includedir /etc/mysql/conf.d/'; } >> /etc/my.cnf
+fi
 mkdir -p /etc/mysql/conf.d
 
 # ensure these directories exist and have useful permissions
@@ -40,7 +55,7 @@ mkdir -p /etc/mysql/conf.d
 mkdir -p /var/lib/mysql /var/run/mysqld
 chown mysql:mysql /var/lib/mysql /var/run/mysqld
 
-# ensure that /var/run/mysqld (used for socket and lock files) is writable regardless of the UID our mysqld instance ends up having at runtime
+# the data directory and /var/run/mysqld (socket and lock files) stay writable whatever UID mysqld runs as
 chmod 1777 /var/lib/mysql /var/run/mysqld
 
 systemctl enable mysqld
